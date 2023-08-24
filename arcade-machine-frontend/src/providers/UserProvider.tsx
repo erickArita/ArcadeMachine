f8c74b2a7280fdf0720755a19ee8eac8218008d3
Erick Arita <erick.emao@gmail.com>
2023-08-23 23:48:52 -0600

    aa

--- arcade-machine-frontend/src/providers/UserProvider.tsx
+++ arcade-machine-frontend/src/providers/UserProvider.tsx
@@ -1,46 +1,45 @@
-import { FC, PropsWithChildren, createContext, useContext } from "react";
-import { Navigate } from "react-router-dom";
-import { useGetUserDataQuery } from "../features/api/autentication/autorizacion";
-import { useAuth } from "../libraries/auth";
-import { AuthenticationResponse } from "../features/api/autentication/models/AutenticationResponse";
-import { RenderIf } from "../components/RenderIf";
-import { Loader } from "../components/Loader";
+import {FC, PropsWithChildren, createContext, useContext} from "react";
+import {Navigate} from "react-router-dom";
+import {useGetUserDataQuery} from "../features/api/autentication/autorizacion";
+import {useAuth} from "../libraries/auth";
+import {AuthenticationResponse} from "../features/api/autentication/models/AutenticationResponse";
+import {Loader} from "../components/Loader";
 
 interface UserProviderProps {
-  user: AuthenticationResponse | undefined;
-  isLoading: boolean;
+    user: AuthenticationResponse | undefined;
+    isLoading: boolean;
 }
 
 const UserContext = createContext<UserProviderProps | null>(null);
 
 export const useUser = () => {
-  const context = useContext(UserContext);
-  if (!context) {
-    throw new Error("useUser debe estar dentro del proveedor UserProvider");
-  }
-  return context;
+    const context = useContext(UserContext);
+    if (!context) {
+        throw new Error("useUser debe estar dentro del proveedor UserProvider");
+    }
+    return context;
 };
 
-export const UserProvider: FC<PropsWithChildren> = ({ children }) => {
-  const { logOut, isLoading } = useAuth();
-  const { data, isFetching, isError } = useGetUserDataQuery(undefined, {
-    skip: isLoading,
-  });
-  console.log(isError);
+export const UserProvider: FC<PropsWithChildren> = ({children}) => {
+    const {logOut, isLoading} = useAuth();
+    const {data, isFetching, isError} = useGetUserDataQuery(undefined, {
+        skip: isLoading,
+    });
+    console.log(isError);
 
-  if (isError && !isLoading) {
-    logOut();
-    return <Navigate to="/" />;
-  }
+    if (isError && !isLoading) {
+        logOut();
+        return <Navigate to="/"/>;
+    }
 
-  return (
-    <UserContext.Provider
-      value={{
-        user: data,
-        isLoading: isFetching,
-      }}
-    >
-      <Loader isLoading={isLoading}>{children}</Loader>
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider
+            value={{
+                user: data,
+                isLoading: isFetching,
+            }}
+        >
+            <Loader isLoading={isLoading}>{children}</Loader>
+        </UserContext.Provider>
+    );
 };
